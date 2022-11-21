98a994fedd30f04fda14a7ec86a87eeb802e099a
Lucas Sancheta <62576196+Lsancheta@users.noreply.github.com>
2022-11-21 16:32:05 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,30 +1,39 @@
-<html>
-    <head>
-        <title>Add Data</title>
-    </head>
-<body>
-    <a href="index.php">HOME</a>
-    <br><br/>
+<?php
 
-    <form action="add.php" method="post" name="form1">
-        <table width="25%" border = "0">
-            <tr>
-                <td>Name</td>
-                <td><input type="text" name="name"></td>
-            </tr>
-            <tr>
-                <td>Age</td>
-                <td><input type="text" name="age"></td>
-            </tr>
-            <tr>
-                <td>Email</td>
-                <td><input type="text" name="email"></td>
-            </tr>
-            <tr>
-                <td></td>
-                <td><input type="submit" name="submit" value="Add"></td>
-            </tr>
-        </table>
-    </form>
+//including the database connnection file
+include_once("config.php");
+
+//fetching data in descending order (lastest entry first)
+//$result= mysql_query("SELECT * FROM users ORDER BY id DESC);
+//mysql_query is deprecated
+$result = mysqli_query($mysqli, "SELECT * FROM users ORDER BY id DESC"); // using mysqli_query instead
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Homepage </title>
+</head>
+<body>
+    <a href="add.html">Add New Data</a><br><br>
+    <table width='80%' border =0>
+        <tr bgcolor='#CCCCCC'>
+            <td>Name</td>
+            <td>Age</td>
+            <td>Email</td>
+            <td>Update</td>
+        </tr>
+        <?php
+        while($res = mysqli_fetch_array($result)){
+            echo "<tr>";
+            echo "<td>".$res['name']."</td>";
+            echo "<td>".$res['age']."</td>";
+            echo "<td>".$res['email']."</td>";
+            echo "<td><a href=\"edit.php?id=$res[id]\">Edit</a> | <a href=\"delete.php?id=$res[id]\" onClick=\"return confirm('Are you sure you want to delete?')\">Delete</a></td>";
+        }
+        ?>
+    </table>
 </body>
 </html>
