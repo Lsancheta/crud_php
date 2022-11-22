67bc2457d9729a1a1d2a2e08c531269a8ecd07b4
Lucas Sancheta <62576196+Lsancheta@users.noreply.github.com>
2022-11-22 14:40:39 -0300

    Update config.php
    fiz uma confusão e o config estava com o arquivo html, agora ta certo informando como configurar o banco.

--- config.php
+++ config.php
@@ -1,30 +1,10 @@
-<html>
-    <head>
-        <title>Add Data</title>
-    </head>
-<body>
-    <a href="index.php">HOME</a>
-    <br><br/>
+<?php
+$databaseHost='localhost';
+$databaseName='crud';
+$databaseUsername='root';
+$databasePassword='';
 
-    <form action="add.php" method="post" name="form1">
-        <table width="25%" border = "0">
-            <tr>
-                <td>Name</td>
-                <td><input type="text" name="name"></td>
-            </tr>
-            <tr>
-                <td>Age</td>
-                <td><input type="text" name="age"></td>
-            </tr>
-            <tr>
-                <td>Email</td>
-                <td><input type="text" name="email"></td>
-            </tr>
-            <tr>
-                <td></td>
-                <td><input type="submit" name="submit" value="Add"></td>
-            </tr>
-        </table>
-    </form>
-</body>
-</html>
+$mysqli = mysqli_connect($databaseHost, $databaseUsername,
+$databasePassword,$databaseName);
+
+?>
